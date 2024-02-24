6f2ddfcc1ef16f79390ef81fe9fc078450b7bfba
dhenr <noobonta@gmail.com>
2024-02-24 19:46:01 -0300

    Alterações no funcionamento do timer

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Variáveis do temporizador
 let timer = null;
-let time = 0;
+let time = 25 * 60; // Definindo o tempo inicial para 25 minutos
 let pomodoroCount = 0;
 
 // Função para formatar o tempo no formato "mm:ss"
@@ -20,65 +20,22 @@ function updateTimer() {
 function messageTimer() {
     const messageElement = document.getElementById('message');
 
-    if (time > 0 && time < 30) {
+    if (timer !== null && time > 0) {
         messageElement.textContent = "Hora da ação!";
-    } else if (time >= 30) {
+    } else if (timer !== null && time <= 0) {
         messageElement.textContent = "Hora de descansar!";
     } else {
-        messageElement.textContent = ""; // Não mostra nada quando o timer estiver zerado
+        messageElement.textContent = ""; // Não mostra nada quando o timer estiver inativo
     }
-}
-
-document.querySelector('#push').onclick = function() {
-    var taskInput = document.querySelector('#newtask input');
-    if (taskInput.value.length == 0) {
-        alert("Por favor, digite o nome da tarefa!")
-    } else {
-        var taskCount = document.querySelectorAll('.task').length;
-        if (taskCount >= 10) {
-            alert("Limite máximo de 10 tarefas atingido!");
-            return;
-        }
-
-        var timerContainer = document.querySelector('.timer-container');
-        var timerContainerMargin = getComputedStyle(timerContainer).marginTop;
-
-        document.querySelector('#tasks').innerHTML += `
-            <div class="task">
-                <span id="taskname">
-                    ${taskInput.value}
-                </span>
-                <button class="delete">
-                    <i class="far fa-trash-alt">X</i>
-                </button>
-            </div>
-        `;
-
-        timerContainer.style.marginTop = timerContainerMargin; // Manter a margem superior do container do timer
-
-        var current_tasks = document.querySelectorAll(".delete");
-        for (var i = 0; i < current_tasks.length; i++) {
-            current_tasks[i].onclick = function() {
-                this.parentNode.remove();
-            }
-        }
-
-        taskInput.value = '';
-    }
-}
-
-document.querySelector('#deleteall').onclick = function() {
-    var tasksContainer = document.querySelector('#tasks');
-    tasksContainer.innerHTML = '';
 }
 
 // Função para iniciar o temporizador
 function startTimer() {
     if (timer === null) {
         timer = setInterval(function () {
-            time++;
+            time--;
             updateTimer();
-            if (time >= 30) { // 25 minutes (1500 seconds)
+            if (time <= 0) {
                 clearInterval(timer);
                 timer = null;
                 updateTimer();
@@ -86,6 +43,7 @@ function startTimer() {
         }, 1000);
     }
 }
+
 
 // Função para parar o temporizador
 function stopTimer() {
